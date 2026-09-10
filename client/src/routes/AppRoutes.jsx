5ebf871074a364fb1a004069c0273a02a61feea7
Daksh S <dakshsiiitr@gmail.com>
2026-09-11 00:41:57 +0530

    feat: Implement authentication and record details components with routing

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,10 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+import AuthCard from "../components/AuthCard.jsx";
 import UploadCard from "../components/UploadCard.jsx";
+import RecordDetails from "../components/RecordDetails.jsx";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<UploadCard />} />
+      {/* 1. Default Route: Login / Register screen */}
+      <Route path="/" element={<AuthCard />} />
+
+      {/* 2. Dashboard Route: Upload land records */}
+      <Route path="/dashboard" element={<UploadCard />} />
+
+      {/* 3. Output Route: Extracted land data */}
+      <Route path="/records" element={<RecordDetails />} />
     </Routes>
   );
 }
